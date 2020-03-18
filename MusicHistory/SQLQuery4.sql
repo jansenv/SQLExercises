504e702cec9c5107321f99c579f450533c415dec
jansenv <jansenv44@gmail.com>
2020-03-18 16:27:53 -0500

    music database practice

---
+++ MusicHistory/SQLQuery4.sql
@@ -0,0 +1,8 @@
+﻿--SELECT Id, Title, SongLength, ReleaseDate, GenreId, ArtistId, AlbumId FROM Song;
+
+--SELECT Id, Title, ReleaseDate FROM Song;
+
+--SELECT * FROM Song;
+
+--SELECT Id, Title, SongLength, ReleaseDate, GenreId, ArtistId, AlbumId FROM Song
+--WHERE SongLength > 100;
