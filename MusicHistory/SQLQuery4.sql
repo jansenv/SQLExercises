01be81b3d1357f2d190804e06d207f6c8395b4b0
jansenv <jansenv44@gmail.com>
2020-03-19 10:34:27 -0500

    finished exercises

--- MusicHistory/SQLQuery4.sql
+++ MusicHistory/SQLQuery4.sql
@@ -43,7 +43,8 @@
 
 --1
 
---SELECT * FROM Genre;
+--SELECT * 
+--FROM Genre;
 
 --2
 
@@ -52,3 +53,58 @@
 --3
 
 --INSERT INTO Album VALUES ('Purple Rain', 3/23/1984, 2100, 'Columbia', 29, 7);
+
+--4
+
+--INSERT INTO Song VALUES ('Darling Nikki', 360, 3/23/1984, 7, 29, 23)
+
+--5
+
+--SELECT s.Title, al.Title, ar.ArtistName 
+--FROM Song s
+--LEFT JOIN Album al ON s.AlbumId = al.Id
+--LEFT JOIN Artist ar ON s.ArtistId = ar.Id
+--WHERE s.Id = 22
+
+--6
+
+--SELECT COUNT (AlbumId) AS 'Song Count', al.Title 
+--FROM Song S
+--LEFT JOIN Album al ON s.AlbumId = al.Id
+--GROUP BY AlbumId, al.Title
+--ORDER BY COUNT(AlbumId) desc;
+
+--7
+
+--SELECT COUNT (ArtistId) AS 'Song Count', ar.ArtistName 
+--FROM Song S
+--LEFT JOIN Artist ar ON s.ArtistId = ar.Id
+--GROUP BY ArtistId, ar.ArtistName
+--ORDER BY COUNT(AlbumId) desc;
+
+--8
+
+--SELECT COUNT (GenreId) AS 'Song Count', g.[Label]
+--FROM Song S
+--LEFT JOIN Genre g ON s.GenreId = g.Id
+--GROUP BY GenreId, g.[Label]
+--ORDER BY COUNT(AlbumId) desc;
+
+--9
+
+--SELECT Title, AlbumLength
+--FROM Album
+--WHERE AlbumLength=(SELECT MAX(AlbumLength) from Album);
+
+--10
+
+--SELECT Title, SongLength
+--FROM Song S
+--WHERE SongLength=(SELECT MAX(SongLength) from Song);
+
+--11
+
+--SELECT s.Title, a.Title, SongLength as 'Song Length'
+--from Song s
+--LEFT JOIN Album a ON s.AlbumId = a.Id
+--WHERE SongLength=(SELECT MAX(SongLength) from Song);
