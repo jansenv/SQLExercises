0abd1e1f0878006744e7c110bcb14682554c4804
jansenv <jansenv44@gmail.com>
2020-03-25 19:50:32 -0500

    finished repo setup

---
+++ WalkinTheDog/WalkinTheDog/Models/Neighborhood.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WalkinTheDog.Models
+{
+    public class Neighborhood
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ WalkinTheDog/WalkinTheDog/Models/Walker.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WalkinTheDog.Models
+{
+    public class Walker
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int NeighborhoodId { get; set; }
+    }
+}
